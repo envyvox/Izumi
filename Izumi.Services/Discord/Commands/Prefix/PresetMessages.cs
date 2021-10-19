f3fc52059fd7f6933c89ee59b33fa2ecdad120fd
envyvox <kkxo.mail@gmail.com>
2021-10-19 11:09:07 +0300

    Preset message with game info.

--- Izumi.Services/Discord/Commands/Prefix/PresetMessages.cs
+++ Izumi.Services/Discord/Commands/Prefix/PresetMessages.cs
@@ -3,6 +3,7 @@ using Discord;
 using Discord.Commands;
 using Izumi.Data.Enums;
 using Izumi.Data.Enums.Discord;
+using Izumi.Services.Discord.Client;
 using Izumi.Services.Discord.Emote.Extensions;
 using Izumi.Services.Discord.Emote.Queries;
 using Izumi.Services.Discord.Guild.Queries;
@@ -18,10 +19,14 @@ namespace Izumi.Services.Discord.Commands.Prefix
     public class PresetMessages : ModuleBase<SocketCommandContext>
     {
         private readonly IMediator _mediator;
+        private readonly IDiscordClientService _discordClientService;
 
-        public PresetMessages(IMediator mediator)
+        public PresetMessages(
+            IMediator mediator,
+            IDiscordClientService discordClientService)
         {
             _mediator = mediator;
+            _discordClientService = discordClientService;
         }
 
         [Command("game-roles")]
@@ -47,7 +52,8 @@ namespace Izumi.Services.Discord.Commands.Prefix
                     .WithMaxValues(14)
                     .AddOption("Genshin Impact", "GenshinImpact", emote: Parse(emotes.GetEmote("GenshinImpact")))
                     .AddOption("League of Legends", "LeagueOfLegends", emote: Parse(emotes.GetEmote("LeagueOfLegends")))
-                    .AddOption("Teamfight Tactics", "TeamfightTactics", emote: Parse(emotes.GetEmote("TeamfightTactics")))
+                    .AddOption("Teamfight Tactics", "TeamfightTactics",
+                        emote: Parse(emotes.GetEmote("TeamfightTactics")))
                     .AddOption("Valorant", "Valorant", emote: Parse(emotes.GetEmote("Valorant")))
                     .AddOption("Apex Legends", "ApexLegends", emote: Parse(emotes.GetEmote("ApexLegends")))
                     .AddOption("Lost Ark", "LostArk", emote: Parse(emotes.GetEmote("LostArk")))
@@ -120,6 +126,90 @@ namespace Izumi.Services.Discord.Commands.Prefix
                     "каналов будут удалены модерацией сервера без предупреждения.");
 
             await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+
+        [Command("game-info")]
+        public async Task SendGameInfoTask()
+        {
+            var client = await _discordClientService.GetSocketClient();
+            var emotes = await _mediator.Send(new GetEmotesQuery());
+            var channels = await _mediator.Send(new GetChannelsQuery());
+
+            await Context.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                .WithDefaultColor()
+                .WithDescription(
+                    $"На нашем сервере присутствует собственный бот {client.CurrentUser.Mention}" +
+                    "\nс упрощенной RPG системой." +
+                    $"\n\n{emotes.GetEmote("Arrow")} Все игровые команды отправляются в текстовый чат <#{channels[DiscordChannelType.Commands].Id}>.")
+                .Build());
+
+            await Context.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                .WithDefaultColor()
+                .WithDescription(
+                    "Мы **рекомендуем** начать знакомство с игровым миром с прохождения `/обучение`.")
+                .WithImageUrl(
+                    "https://cdn.discordapp.com/attachments/842067362139209778/899923888224468992/6b30851aad3530d0.png")
+                .Build());
+
+            await Context.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                .WithDefaultColor()
+                .WithDescription("В процессе прохождения **обучения** вы узнаете про:")
+                .Build());
+
+            await Context.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                .WithDefaultColor()
+                .WithDescription(
+                    $"{emotes.GetEmote("List")} **Исследования** локаций, добывая различные " +
+                    $"{emotes.GetEmote("Flax")}{emotes.GetEmote("Wood")}{emotes.GetEmote("Gold")}{emotes.GetEmote("Coal")} ресурсы;")
+                .WithImageUrl(
+                    "https://cdn.discordapp.com/attachments/842067362139209778/899925204036362241/b77fb0bfce1c814f.png")
+                .Build());
+
+            await Context.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                .WithDefaultColor()
+                .WithDescription(
+                    $"{emotes.GetEmote("List")} **Рыбалку**, где можно поймать различную " +
+                    $"{emotes.GetEmote("Octopus")} рыбу на {emotes.GetEmote("Ien")} продажу или переработку;")
+                .WithImageUrl(
+                    "https://cdn.discordapp.com/attachments/842067362139209778/899925662461206549/3e6494245538e61b.png")
+                .Build());
+
+            await Context.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                .WithDefaultColor()
+                .WithDescription(
+                    $"{emotes.GetEmote("List")} Разнообразные **магазины** для приобретения " +
+                    $"{emotes.GetEmote("BlueberrySeeds")}{emotes.GetEmote("GrapeSeeds")} семян, {emotes.GetEmote("Egg")} продуктов, " +
+                    $"{emotes.GetEmote("Recipe")} рецептов блюд, баннеров для профиля и т.д.;")
+                .WithImageUrl(
+                    "https://cdn.discordapp.com/attachments/842067362139209778/899923884567068692/ec6b7bcffbf1a1d3.gif")
+                .Build());
+
+            await Context.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                .WithDefaultColor()
+                .WithDescription(
+                    $"{emotes.GetEmote("List")} **Участок**, где можно выращивать " +
+                    $"{emotes.GetEmote("Blueberry")}{emotes.GetEmote("Grape")} урожай для приготовления " +
+                    $"{emotes.GetEmote("BlueberryTart")} блюд или же просто для продажи на рынке;")
+                .WithImageUrl(
+                    "https://cdn.discordapp.com/attachments/842067362139209778/899923906142556190/cd952a99a3ff5b23.png")
+                .Build());
+
+            await Context.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                .WithDefaultColor()
+                .WithDescription(
+                    $"{emotes.GetEmote("List")} **Казино** и {emotes.GetEmote("LotteryTicket")} лотерею;")
+                .WithImageUrl(
+                    "https://cdn.discordapp.com/attachments/842067362139209778/899923901474283560/5932a15bff178d2f.png")
+                .Build());
+
+            await Context.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                .WithDefaultColor()
+                .WithDescription(
+                    $"{emotes.GetEmote("List")} **Репутацию** городов, которую можно повысить за счет " +
+                    "выполнения рабочих контрактов и победы над ежедневным боссом;")
+                .WithImageUrl(
+                    "https://cdn.discordapp.com/attachments/842067362139209778/899923892192296980/ae537eb1e5dc6b37.png")
+                .Build());
         }
     }
 }
